81738d2e40cb566c739cc65b94709387f23c3ecc
alexpryp <alexpryp@gmail.com>
2018-08-07 01:56:07 +0300

    A simple example of an array-based stack

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-function Stack( capacity ) {
+/*function Stack( capacity ) {
 	this._capacity = capacity || Infinity;
 	this._size = 0;
 	this._stack = {};
@@ -39,7 +39,7 @@ Stack.prototype.getSize = function() {
 
 
 
-/*Testing*/
+
 var myStack = new Stack();
 
 myStack.push( "a" );
@@ -57,3 +57,48 @@ console.log( myStack._size );
 console.log( myStack.pop() );
 console.log( myStack._size );
 console.log( myStack.getSize() );
+*/
+
+
+
+function Stack(capacity) {
+    this._capacity = capacity || Infinity;
+    this._stack = [];
+}
+
+Stack.prototype.push = function(item) {
+    if ( this._stack.length < this._capacity ) {
+        this._stack.push(item);
+        return this._stack.length;
+    }
+    return"Stack is full";
+};
+
+Stack.prototype.pop = function() {
+    if (this._stack.length < 1) {
+        return "stack is ampty";
+    }
+    return this._stack.pop();
+};
+
+
+
+
+/*Testing*/
+Stack.prototype.getSize = function() {
+    return this._stack.length;
+};
+
+var stack = new Stack(100);
+
+console.log(stack._capacity);
+console.log(stack._stack.length);
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.push(40);
+console.log(stack._stack.length);
+console.log(stack.pop());
+console.log(stack._stack.length);
+console.log(stack.pop());
+console.log(stack.getSize());
